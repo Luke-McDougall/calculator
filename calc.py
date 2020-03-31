dd59ec29cfc090dd4d09cadd9958cf1a3dc581c2
Luke McDougall <lukeyr10@gmail.com>
2020-03-31 08:05:39 +0800

    goobtoob

--- calc.py
+++ calc.py
@@ -44,46 +44,8 @@ def degrees(x):
 def radians(x):
     return math.radians(x)
 
-def column_multiply(x, y):
-    x_list = []
-    y_list = []
-    x_list.append(x)
-    y_list.append(y)
-
-    index = 0
-    while x_list[index] > 1:
-        x_list.append(x_list[index] // 2)
-        y_list.append(y_list[index] * 2)
-        index += 1
-
-    result = 0
-    for a, b in zip(x_list, y_list):
-        if a & 1:
-            result += b
-
-    return result
-
-
 def log(*args):
     return math.log(*args)
-
-def is_prime(n):
-    if n % 2 == 0:
-        return False 
-    x = 2
-    while x * x <= n:
-        if n % x == 0:
-            return False
-        x += 1
-    return True
-
-def next_prime(n):
-    if n <= 2:
-        return 2
-
-    while not is_prime(n):
-        n += 1
-    return n
 
 def lcm(x, y):
     return (x * y) / gcd(x, y)
